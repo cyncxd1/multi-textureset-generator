0eaaa7d40c4acb1bef8f0d8af506a7a645869435
cyncxd <79019625+cyncxd1@users.noreply.github.com>
2024-09-16 15:11:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,42 +3,45 @@ import os
 import json
 import shutil
 
-# insert path for your texture files here.
-# *make sure to use a valid folder path
-path = r'Your path here'
+# Insert path for your texture files here.
+path = r'C:\Users\cyncxd\Desktop\block'
 files = os.listdir(path)
 
-main_dir = os.path.realpath(__file__)
-output_files = os.listdir(main_dir[:-7])
+# Get the directory of the script
+main_dir = os.path.dirname(os.path.abspath(__file__))
+output_files_dir = os.path.join(main_dir, path)  # Adjust as necessary
 
 # MER and normal suffixes
-# *do not include anything besides the suffix
 merSuffix = "mer"
 normalSuffix = "normal"
-# choose type of depth map(heightmap or normal)
 depthType = "normal"
 
-#* DO NOT CHANGE ANYTHING HERE
-def textureSet(texture_name, mer_suffix, normal_suffix, depth_type):
+def textureSet(texture, mer_suffix, normal_suffix, depth_type):
     texture_set = {
         "format_version": "1.16.100",
-        "color": texture_name,
-        "metalness_emissive_roughness": texture_name + "_" + mer_suffix,
-        depth_type: texture_name + "_" + normal_suffix
+        "color": texture,
+        "metalness_emissive_roughness": texture + "_" + mer_suffix,
+        depth_type: texture + "_" + normal_suffix
     }
-    with open(texture_name + ".texture_set.json", "w") as name:
+    json_file_path = os.path.join(output_files_dir, texture + ".texture_set.json")
+    with open(json_file_path, "w", encoding="utf-8") as name:
         json.dump(texture_set, name, indent=4)
+    print(f"Created JSON file: {json_file_path}")
 
+# Create texture set JSON files
 for item in files:
-    if (not ("_" + merSuffix + ".png" in item or "_" + normalSuffix + ".png" in item) and ".png" in item):
-        print(item[:-4])
-        textureSet(item[:-4], merSuffix, normalSuffix, depthType)
-        
+    hasSuffix = "_" + merSuffix + ".png" in item or "_" + normalSuffix + ".png" in item
+    if (not (hasSuffix) and ".png" in item):
+        texture_name = item[:-4]
+        textureSet(texture_name, merSuffix, normalSuffix, depthType)
 
-for item in output_files:
-    if (".json" in item):
-        shutil.move(os.path.join(main_dir[:-7], item), path)
-        
-print(main_dir[:-7])
+# Move JSON files to the specified path
+output_files = os.listdir(output_files_dir)
+for texture in output_files:
+    if '.json' in texture:
+        source_path = os.path.join(output_files_dir, texture)
+        destination_path = os.path.join(path, texture)
+        shutil.move(source_path, destination_path)
+        print(f"Moved {texture} to {path}")
 
 sys.exit()
