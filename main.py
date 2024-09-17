a4178b7824786c0d2f3489a45669e608b6cc7c4b
cyncxd <79019625+cyncxd1@users.noreply.github.com>
2024-09-17 14:25:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import shutil
 
 # Insert path for your texture files here.
-PATH = r'C:\Users\cyncxd\Desktop\block'
+PATH = r'Your path here'
 files = os.listdir(PATH)
 
 # Get the directory of the script
